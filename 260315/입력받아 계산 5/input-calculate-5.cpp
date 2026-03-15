e0e4e40de7ea4fc105dfb62dd9bcb845ae111aa7
20201865 문연지 <145094014+YeonjiMun@users.noreply.github.com>
2026-03-15 21:29:16 +0900

    Title: 입력받아 계산 5, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260315/입력받아 계산 5/input-calculate-5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    1<=a,b<=100;
+
+    cin >> a >> b;
+    cout << a+b;
+    
+    return 0;
+}

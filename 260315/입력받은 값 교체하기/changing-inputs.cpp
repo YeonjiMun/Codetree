3dc78327ab12d05c38df5412dfafafd809e4754d
20201865 문연지 <145094014+YeonjiMun@users.noreply.github.com>
2026-03-15 21:32:22 +0900

    Title: 입력받은 값 교체하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260315/입력받은 값 교체하기/changing-inputs.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    int temp;
+
+    1<=a,b<=100;
+
+    cin >> a >> b;
+    cout << b << " " << a;
+    return 0;
+}

988fce6386178cb60fff0756766f383ecadc0c98
20201865 문연지 <145094014+YeonjiMun@users.noreply.github.com>
2026-03-16 17:20:40 +0900

    Title: 실수와 문자 받아 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260316/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a, b;
+    string c;
+
+    1<=a,b<=1000;
+
+    cout << fixed;
+    cout.precision(2);
+
+    cin >> c >> a >> b;
+    cout << c << "\n" << a << "\n" << b;
+
+    return 0;
+}

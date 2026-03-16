599de87772862f497fec7575b67d502e17f0dc4d
20201865 문연지 <145094014+YeonjiMun@users.noreply.github.com>
2026-03-16 16:51:01 +0900

    Title: 세 실수의 반올림, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260316/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a, b, c;
+    0<=a, b, c<= 1000;
+    cin >> a >> b>> c;
+    
+    return 0;
+}

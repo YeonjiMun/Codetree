8162d6f1715d9ba5e9eb4e56563af476e052aae3
20201865 문연지 <145094014+YeonjiMun@users.noreply.github.com>
2026-03-18 18:36:32 +0900

    Title: 전화번호 바꾸기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260318/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a, b, c;
+    char d;
+
+    cin >> a >> d >> b >> d >> c;
+    cout << "010" << "-" << c << "-" << b;
+    return 0;
+}
